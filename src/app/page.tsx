b4cf87b6084226c3cd3eccf1a479a92a7e50df93
designdevhybrid <abhishekkalia.productdesign@gmail.com>
2023-05-15 18:08:53 -0400

    test 2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,14 +8,14 @@ export default function Home() {
     <main className="flex h-screen flex-col py-8 px-4 max-w-xl justify-around">
       <div className="top">
         <MainAnimation />
-        {/* <Title intent="title" alignment="centered">
+        <Title intent="title" alignment="centered">
           Welcome to Journi!
         </Title>
         <Paragraph alignment="centered">
           Journi is a tiny everyday journal that helps you build a journaling
           habit by asking you 3 questions on how you are feeling.
         </Paragraph>
-        <Button className="mt-24">Start Journaling</Button> */}
+        <Button className="mt-24">Start Journaling</Button>
       </div>
     </main>
   );
